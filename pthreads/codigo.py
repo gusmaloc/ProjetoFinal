ec9c3affabbc96dc078ed717cabc531ef45d164b
vitoriakarpovas <64480334+vitoriakarpovas@users.noreply.github.com>
2022-05-09 17:23:36 -0300

    Create codigo.py

---
+++ pthreads/codigo.py
@@ -0,0 +1,28 @@
+import _thread
+
+num_thread = 0
+global soma
+thread_started = False
+
+def taylor(max_loop, num_min):
+    global num_thread, thread_started, soma
+    thread_started = True
+    num_thread += 1
+    while num_min <= max_loop:
+        soma += 1/num_min
+        num_min += 1
+    num_thread -= 1
+
+soma = 0        
+_thread.start_new_thread(taylor, (5, 1))
+_thread.start_new_thread(taylor, (10, 6))
+
+while not thread_started:
+    pass
+
+print("Soma durante o processo: ", soma)
+ 
+while num_thread > 0:
+    pass
+
+print("Soma final: ", soma)
