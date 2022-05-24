40d3362a99da2e1c111dda5ae815338566263cb3
TGHP0 <90909985+TGHP0@users.noreply.github.com>
2022-05-23 21:10:36 -0300

    Update multiprocessing.py

--- Projeto V3/multiprocessing.py
+++ Projeto V3/multiprocessing.py
@@ -1,43 +1,31 @@
-import _thread
-import multiprocessing as mp
 
-num_thread = 4
-swift = 0
-start = 1
-end = 250
-global global_result
-thread_started = False
+import multiprocessing
+
 global_result = 0
 
-def taylor(start_num, end_num, q):
-    global thread_started, swift, global_result
-    thread_started = True
+def taylor(start_num, end_num):
+    global global_result
     T = start_num
-    swift += 1
     while T <= end_num:
         global_result += 1/T
         T += 1
-    swift -= 1
 
 
-if __name__ == '__main__':
-    ctx = mp.get_context('spawn')
-    q = ctx.Queue()
-    p = ctx.Process(target=taylor, args=(start, swift, q))
-    p.start()
-    for i in range(num_thread):
-        _thread.start_new_thread(taylor, (start, end))
-        start += 250
-        end += 250
-    p.join()
 
 
-while not thread_started:
-    pass
-
-print("Resultado Parcial: ", global_result)
-
-while swift > 0:
-    pass
+p1 = multiprocessing.Process(target = taylor(1,250))
+p2 = multiprocessing.Process(target = taylor(251,500) )
+p3 = multiprocessing.Process(target = taylor(501,750))
+p4 = multiprocessing.Process(target = taylor(751,1000))
+    
+p1.start()
+p2.start()
+p3.start()
+p4.start()
+    
+p1.join()
+p2.join()
+p3.join()
+p4.join()
 
 print("\nResultados: ", global_result)
