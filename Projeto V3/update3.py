2af67c92e8cfdaafe4e1851540f06d11728a8698
vitoriakarpovas <64480334+vitoriakarpovas@users.noreply.github.com>
2022-05-26 09:51:13 -0300

    Create update3.py

---
+++ Projeto V3/update3.py
@@ -0,0 +1,18 @@
+import multiprocessing as mp
+from multiprocessing import Pool
+from functools import reduce
+
+global_result = 0
+
+def taylor(x):
+    #global global_result
+    #global_result += 1/x
+    return 1/x
+
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    with Pool(10) as p:
+        global_result = p.map(taylor,range(1,1000))
+    result = reduce(lambda x,y: x+y ,global_result)
+
+    print("resultado: ", result)
